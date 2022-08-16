375d6dac50531ba8c2ed5fb884d6dde1e9944714
[ganapat] <ganapatisakatwa@gmail.com>
2022-08-16 11:10:01 +0530

    [Ganapati]Fibonacci Series

---
+++ Logical_programming/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Logical_programming
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+
+            Fibonacci_Series_child obj = new Fibonacci_Series_child();
+
+            obj.Series();
+            obj.Display();
+
+        }
+    }
+}
